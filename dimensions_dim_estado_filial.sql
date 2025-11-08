85c7611560fd57f4ce4fc47317ddd68b7636b428
Martin <103909790+mnieves79@users.noreply.github.com>
2025-11-08 17:00:08 -0500

    Clean up SQL dump by removing setup statements
    Removed unnecessary database creation and setup statements from SQL dump.

--- dimensions_dim_estado_filial.sql
+++ dimensions_dim_estado_filial.sql
@@ -1,29 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `dimensions` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `dimensions`;
--- MySQL dump 10.13  Distrib 8.0.34, for macos13 (arm64)
---
--- Host: 127.0.0.1    Database: dimensions
--- ------------------------------------------------------
--- Server version	8.0.26
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `dim_estado_filial`
---
-
-DROP TABLE IF EXISTS `dim_estado_filial`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `dim_estado_filial` (
   `sexo_cd` varchar(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `estado_filial` varchar(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT 'Hijo Legitimo o Natural',
@@ -31,25 +5,7 @@ CREATE TABLE `dim_estado_filial` (
   `id` int NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+;
 
---
--- Dumping data for table `dim_estado_filial`
---
-
-LOCK TABLES `dim_estado_filial` WRITE;
-/*!40000 ALTER TABLE `dim_estado_filial` DISABLE KEYS */;
-INSERT INTO `dim_estado_filial` VALUES ('V','HL','hijo legitimo',1),('H','HL','hija legitima',2),('H','HN','hija natural',3),('V','HN','hijo natural',4),('V','NC','hijo',5),('H','NC','hija',6),('V','ES','niño exposito',7),('H','ES','niña exposito',8),('H','[---]','hija [---]',9),('V','[---]','hijo [---]',10),('*','[---]','* [---]',11),('V','HI','hijo ilegitimo',12),('H','HI','hija ilegitima',13),('V','IG','hijo de la iglesia',14),('H','IG','hija de la iglesia',15);
-/*!40000 ALTER TABLE `dim_estado_filial` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2025-11-08 16:48:54
+INSERT INTO `dim_estado_filial` 
+  VALUES ('V','HL','hijo legitimo',1),('H','HL','hija legitima',2),('H','HN','hija natural',3),('V','HN','hijo natural',4),('V','NC','hijo',5),('H','NC','hija',6),('V','ES','niño exposito',7),('H','ES','niña exposito',8),('H','[---]','hija [---]',9),('V','[---]','hijo [---]',10),('*','[---]','* [---]',11),('V','HI','hijo ilegitimo',12),('H','HI','hija ilegitima',13),('V','IG','hijo de la iglesia',14),('H','IG','hija de la iglesia',15);
